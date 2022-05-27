b13d86347bee976b716dc91454f18747ef6c367c
Christian Seidel <github@parameter5.de>
2022-05-27 09:14:28 +0200

    kata 01 solved

--- src/main/java/codewars/fundamentals/Kata01.java
+++ src/main/java/codewars/fundamentals/Kata01.java
@@ -2,7 +2,7 @@ package codewars.fundamentals;
 
 public class Kata01 {
 
-    // YET UNFINISHED //
+    // UNFINISHED //
 
     /* Sum all the numbers of a given array ( cq. list ), except
     the highest and the lowest element (by value, not by index).
@@ -14,23 +14,39 @@ public class Kata01 {
     an array, or the given array is an empty list or a list with
     only 1 element, return 0.   */
 
+    public static int sum01(int[] numbers) {
+        if (numbers == null || numbers.length < 2) return 0;
 
-    public static int sum(int[] numbers) {
-        if (numbers == null || numbers.length < 2) {
+        int largestNumber = numbers[0];
+        int smallestNumber = numbers[0];
+        int sum = 0;
+
+        for (int n : numbers) {
+            sum = sum + n;
+            if (largestNumber < n) largestNumber = n;
+            if (smallestNumber > n) smallestNumber = n;
+        }
+
+        sum = sum - largestNumber - smallestNumber;
+        return sum;
+    }
+
+    public static int sum02(int[] numbersB) {
+        if (numbersB == null || numbersB.length < 2) {
             return 0;
         }
         int swap;
         int i = 1;
-        while (numbers[i - 1] > numbers[i]) {
-            swap = numbers[i - 1];
-            numbers[i - 1] = numbers[i];
-            numbers[i] = swap;
+        while (numbersB[i - 1] > numbersB[i]) {
+            swap = numbersB[i - 1];
+            numbersB[i - 1] = numbersB[i];
+            numbersB[i] = swap;
             i++;
         }
 
         int sum = 0;
-        for (int j = 1; j < (numbers.length - j); j++) {
-            sum = sum + numbers[j];
+        for (int j = 1; j < (numbersB.length - j); j++) {
+            sum = sum + numbersB[j];
         }
 
         return sum;
--- src/test/java/codewars/fundamentals/Kata01Test.java
+++ src/test/java/codewars/fundamentals/Kata01Test.java
@@ -4,22 +4,32 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class KataSumTest {
+class Kata01Test {
 
 
     @Test
     public void ShouldReturnZeroBecauseOneElementOnly() {
-        assertEquals(0, Kata01.sum(new int[] {7}));
+        assertEquals(0, Kata01.sum01(new int[] {7}));
     }
 
     @Test
     public void ShouldReturnZeroBecauseEmpty() {
-        assertEquals(0, Kata01.sum(new int[] {}));
+        assertEquals(0, Kata01.sum01(new int[] {}));
     }
 
     @Test
-    public void BasicTests() {
-        assertEquals(16, Kata01.sum(new int[] { 6, 2, 1, 8, 10}));
+    public void BasicTest() {
+        assertEquals(16, Kata01.sum01(new int[] {6, 2, 1, 8, 10}));
+    }
+
+    @Test
+    public void TestWithSameNumberTwiceA() {
+        assertEquals(37, Kata01.sum01(new int[] {6, 11, 11, 2, 1, 8, 10}));
+    }
+
+    @Test
+    public void TestWithSameNumberTwiceB() {
+        assertEquals(65, Kata01.sum01(new int[] {17, 6, 11, 11, 2, 1, 8, 10, 17}));
     }
 
 }
