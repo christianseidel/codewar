9c1d69d6869f4f68e8b0e9aa970854715cbff350
Christian Seidel <github@parameter5.de>
2022-06-02 16:52:19 +0200

    +allIntDoubled

---
+++ src/test/java/codewars/fundamentals/AllIntDoubledTest.java
@@ -0,0 +1,19 @@
+package codewars.fundamentals;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AllIntDoubledTest {
+    
+    @Test
+    public void firstSimpleTest() {
+        assertArrayEquals(new int[] {2, 4, 6}, AllIntDoubled.map(new int[] {1, 2, 3}));
+    }
+    
+    @Test
+    public void sampleTests() {
+        assertArrayEquals(new int[] {8, 2, 2, 2, 8}, AllIntDoubled.map(new int[] {4, 1, 1, 1, 4}));
+        assertArrayEquals(new int[] {2, 2, 2, 2, 2, 2}, AllIntDoubled.map(new int[] {1, 1, 1, 1, 1, 1}));
+    }
+}
