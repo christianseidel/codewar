1bc9f17864708862a5cdb1d7e9dfc010b7f33d27
Christian Seidel <github@parameter5.de>
2022-06-05 15:46:38 +0200

    Sum Of Intervals: New Start

--- src/main/java/codewars/rankup/SumOfIntervals.java
+++ src/main/java/codewars/rankup/SumOfIntervals.java
@@ -38,44 +38,150 @@ public class SumOfIntervals {
                 });  // [1,8] => 7
  */
 
-    public static int sumIntervals(int[][] intervals) {
+    public int sumIntervals(int[][] intervals) {
 
         if ((intervals == null) || (intervals.length == 0)) {
             return 0;
         }
+        int sum = 0;
+        int[][] sortedIntervals = sortIntervals (intervals);
+        int[][] ledger = establishLedger (sortedIntervals);
 
-        // Intervalls will first be sorted according to their first value.
+        while (countRemainingIntervals(ledger) > 0) {
+            System.out.println(Arrays.deepToString(ledger));
+            ledger = eliminateOneLevelOfOverlap(ledger);
+            System.out.println(Arrays.deepToString(ledger));
+            int numberOfRemaining = countRemainingIntervals(ledger);
+            int[][] nextLedger = new int[numberOfRemaining][4];
+            int indexNextLedger = 0;
+            // eliminate all entries with index 4 = 0;
+            for (int p = 0; p < numberOfRemaining; p++) {
+                if (ledger[p][4] == 0) {
+                    sum = sum + ledger[p][3];
+                } else {
+                    nextLedger[indexNextLedger] = ledger[p];
+                    indexNextLedger++;
+                }
+            }
+            ledger = nextLedger;
+        }
+        return sum;
+    }
+
+    private int[][] sortIntervals (int[][] intervals) {
+        // Intervalls will be sorted according to their first (lower) value.
         for (int i = 0; i < intervals.length; i++) {
             int j = i;
-            while (((j + 1) < intervals.length) && (intervals[j][0] > intervals[j+1][0])) {
+            while (((j + 1) < intervals.length) && (intervals[j][0] > intervals[j + 1][0])) {
                 int zero = intervals[j][0];
                 int one = intervals[j][1];
-                intervals[j][0] = intervals[j+1][0];
-                intervals[j][1] = intervals[j+1][1];
-                intervals[j+1][0] = zero;
-                intervals[j+1][1] = one;
+                intervals[j][0] = intervals[j + 1][0];
+                intervals[j][1] = intervals[j + 1][1];
+                intervals[j + 1][0] = zero;
+                intervals[j + 1][1] = one;
                 if (j == 0) {
                     break;
-                    } else {
+                } else {
                     j--;
                 }
             }
         }
+        return intervals;
+    }
+
+    private int[][] establishLedger (int[][] intervals) {
+        int[][] ledger = new int[intervals.length][4];
+        for (int k = 0; k < intervals.length; k++) {
+            ledger[k][0] = intervals[k][0];
+            ledger[k][1] = intervals[k][1];
+            ledger[k][2] = intervals[k][1] - intervals[k][0];  // This is the (remaining) length of interval.
+            ledger[k][3] = 1;  // Index 3 will serve as a Boolean where 0 = false and 1 = true.
+        }
+        return ledger;
+    }
+
+    private int countRemainingIntervals(int[][] ledger) {
+        int checkSum = 0;
+        for (int p = 0; p < ledger.length; p++) {
+            checkSum = checkSum + ledger[p][2];
+        }
+        return checkSum;
+    }
+
+    private int[][] eliminateOneLevelOfOverlap(int[][] ledger) {
+        for (int m = 0; m < ledger.length; m++) {
+            // difference between start of next and end of current interval
+            int difference = ledger[m+1][0] - ledger [m][1];
+            // check if there is overlap
+            if (difference < 0) {
+                ledger[m+1][0] = ledger[m][1];                  // if there is overlap
+                ledger[m+1][2] = ledger[m+1][2] + difference;   // m+1 gets shortened
+            } else {
+                ledger[m][3] = 0;                               // if not index 3 = 0 meaning 'false'
+            }
+        }
+        return ledger;
+    }
+
+    /*
+
+        //int[] ledger = new int[4];
+        int uniquelyOnwedLengths[] = new int[intervals.length];
+
+        for (int k = 0; k < intervals.length; k++) {
+            // length of element k is owned by k:
+            uniquelyOnwedLengths[k] = intervals[k][1] - intervals[k][0];
+
+            // any upcoming overlap will solely be owned by k.
+            // check if there is overlap at all
+            if (intervals[k+1][0] < intervals[k][1]) {
+                // Start of next is smaller than end of current. Thus, overlap is to be handled:
+                int upperLimit = intervals[k][1]; // set upper limit
+                int nextIntervalTested = k + 1;
+                // as long as there is overlap:
+                while (intervals[nextIntervalTested][0] > upperLimit) {  // start of next is smaller than end of current
+                    uniquelyOnwedLengths[nextIntervalTested] = Math.max((intervals[nextIntervalTested][1] - intervals[k][1]), 0);
+                    nextIntervalTested++;
+                }
+                k = nextIntervalTested - 1;
+            }
+
+
+                // handling overlap
+
+
+                int m = k;
+                // common length will be subtracted
+
+                // handling overlap
+            }
+
+            int endOfItemK = intervals[k][1];
+            int m = 1;
+            while(intervals[k+m][0] < endOfItemK) {
+
+
+            }
+        }
+
 
         // Single intervalls will then be calculated and retained.
-        //// int[][] ledger = new int[intervals.length][3];
         int lengths = 0;
         for (int k = 0; k < intervals.length; k++) {
             lengths = lengths + (intervals[k][1] - intervals[k][0]);
             if (k == intervals.length - 1) {
                 break;
             } else {
-                if (intervals[k][1] > intervals[k+1][0]) {
+                // delete intervals which are outreached on both sides by their preceding interval
+                if ((intervals[k+1][0] > intervals[k][0]) && (intervals[k+1][1] < intervals[k][1])) {
+                    lengths = lengths - (intervals[k+1][1] - intervals[k+1][0]);
+                    k++;
+                } else if (intervals[k][1] > intervals[k+1][0]) {
                     intervals[k+1][0] = intervals[k][1];
                 }
             }
         }
-        return lengths;
-    }
+
+     */
 
 }
--- src/test/java/codewars/rankup/SumOfIntervalsTest.java
+++ src/test/java/codewars/rankup/SumOfIntervalsTest.java
@@ -16,9 +16,10 @@ class SumOfIntervalsTest {
         int [] b = {6, 8};
         int [] c = {11, 15};
         int[][] firstArray = {a, b, c};
+        SumOfIntervals sumIntervals = new SumOfIntervals();
 
         // when
-        int actual = SumOfIntervals.sumIntervals(firstArray);
+        int actual = sumIntervals.sumIntervals(firstArray);
 
         //then
         assertEquals(8, actual);
@@ -32,9 +33,10 @@ class SumOfIntervalsTest {
         int [] b = {17, 30};
         int [] c = {11, 15};
         int[][] firstArray = {a, b, c};
+        SumOfIntervals sumIntervals = new SumOfIntervals();
 
         // when
-        int actual = SumOfIntervals.sumIntervals(firstArray);
+        int actual = sumIntervals.sumIntervals(firstArray);
 
         //then
         assertEquals(18, actual);
@@ -50,9 +52,10 @@ class SumOfIntervalsTest {
         int [] d = {1, 5};
         int [] e = {80, 90};
         int[][] firstArray = {a, b, c, d, e};
+        SumOfIntervals sumIntervals = new SumOfIntervals();
 
         // when
-        int actual = SumOfIntervals.sumIntervals(firstArray);
+        int actual = sumIntervals.sumIntervals(firstArray);
 
         //then
         assertEquals(33, actual);
@@ -60,48 +63,55 @@ class SumOfIntervalsTest {
 
     @Test
     public void shouldHandleNullOrEmptyIntervals() {
-        assertEquals(0, SumOfIntervals.sumIntervals(null));
-        assertEquals(0, SumOfIntervals.sumIntervals(new int[][]{}));
-        assertEquals(0, SumOfIntervals.sumIntervals(new int[][]{{4, 4}, {6, 6}, {8, 8}}));
+        SumOfIntervals sumIntervals = new SumOfIntervals();
+        assertEquals(0, sumIntervals.sumIntervals(null));
+        assertEquals(0, sumIntervals.sumIntervals(new int[][]{}));
+        assertEquals(0, sumIntervals.sumIntervals(new int[][]{{4, 4}, {6, 6}, {8, 8}}));
     }
 
     @Test
     public void shouldHandleEmptyIntervalsAfterFullIntervals() {
-        assertEquals(16, SumOfIntervals.sumIntervals(new int[][]{{4, 5}, {0, 0}, {18, 33}}));
+        SumOfIntervals sumIntervals = new SumOfIntervals();
+        assertEquals(16, sumIntervals.sumIntervals(new int[][]{{4, 5}, {0, 0}, {18, 33}}));
     }
 
     @Test
-    public void shouldHandleInveralOverlappingOnBothSides() {
-        assertEquals(8, SumOfIntervals.sumIntervals(new int[][]{{4, 5}, {7, 11}, {5, 12}}));
-        assertEquals(8, SumOfIntervals.sumIntervals(new int[][]{{4, 5}, {5, 12}, {7, 11}}));
+    public void shouldHandleIntervalOverlappingOnBothSides() {
+        SumOfIntervals sumIntervals = new SumOfIntervals();
+        assertEquals(8, sumIntervals.sumIntervals(new int[][]{{4, 5}, {7, 11}, {5, 12}}));
+        assertEquals(8, sumIntervals.sumIntervals(new int[][]{{4, 5}, {5, 12}, {7, 11}}));
     }
 
     @Test
     public void shouldAddDisjointIntervals() {
-        assertEquals(9, SumOfIntervals.sumIntervals(new int[][]{{1, 2}, {6, 10}, {11, 15}}));
-        assertEquals(11, SumOfIntervals.sumIntervals(new int[][]{{4, 8}, {9, 10}, {15, 21}}));
-        assertEquals(7, SumOfIntervals.sumIntervals(new int[][]{{-1, 4}, {-5, -3}}));
-        assertEquals(78, SumOfIntervals.sumIntervals(new int[][]{{-245, -218}, {-194, -179}, {-155, -119}}));
+        SumOfIntervals sumIntervals = new SumOfIntervals();
+        assertEquals(9, sumIntervals.sumIntervals(new int[][]{{1, 2}, {6, 10}, {11, 15}}));
+        assertEquals(11, sumIntervals.sumIntervals(new int[][]{{4, 8}, {9, 10}, {15, 21}}));
+        assertEquals(7, sumIntervals.sumIntervals(new int[][]{{-1, 4}, {-5, -3}}));
+        assertEquals(78, sumIntervals.sumIntervals(new int[][]{{-245, -218}, {-194, -179}, {-155, -119}}));
     }
 
     @Test
     public void shouldAddAdjacentIntervals() {
-        assertEquals(54, SumOfIntervals.sumIntervals(new int[][]{{1, 2}, {2, 6}, {6, 55}}));
-        assertEquals(23, SumOfIntervals.sumIntervals(new int[][]{{-2, -1}, {-1, 0}, {0, 21}}));
+        SumOfIntervals sumIntervals = new SumOfIntervals();
+        assertEquals(54, sumIntervals.sumIntervals(new int[][]{{1, 2}, {2, 6}, {6, 55}}));
+        assertEquals(23, sumIntervals.sumIntervals(new int[][]{{-2, -1}, {-1, 0}, {0, 21}}));
     }
 
     @Test
     public void shouldAddOverlappingIntervals() {
-        assertEquals(7, SumOfIntervals.sumIntervals(new int[][]{{1, 4}, {7, 10}, {3, 5}}));
-        assertEquals(6, SumOfIntervals.sumIntervals(new int[][]{{5, 8}, {3, 6}, {1, 2}}));
-        assertEquals(19, SumOfIntervals.sumIntervals(new int[][]{{1, 5}, {10, 20}, {1, 6}, {16, 19}, {5, 11}}));
+        SumOfIntervals sumIntervals = new SumOfIntervals();
+        assertEquals(7, sumIntervals.sumIntervals(new int[][]{{1, 4}, {7, 10}, {3, 5}}));
+        assertEquals(6, sumIntervals.sumIntervals(new int[][]{{5, 8}, {3, 6}, {1, 2}}));
+        assertEquals(19, sumIntervals.sumIntervals(new int[][]{{1, 5}, {10, 20}, {1, 6}, {16, 19}, {5, 11}}));
     }
 
     @Test
     public void shouldHandleMixedIntervals() {
-        assertEquals(13, SumOfIntervals.sumIntervals(new int[][]{{2, 5}, {-1, 2}, {-40, -35}, {6, 8}}));
-        assertEquals(1234, SumOfIntervals.sumIntervals(new int[][]{{-7, 8}, {-2, 10}, {5, 15}, {2000, 3150}, {-5400, -5338}}));
-        assertEquals(158, SumOfIntervals.sumIntervals(new int[][]{{-101, 24}, {-35, 27}, {27, 53}, {-105, 20}, {-36, 26},}));
+        SumOfIntervals sumIntervals = new SumOfIntervals();
+        assertEquals(13, sumIntervals.sumIntervals(new int[][]{{2, 5}, {-1, 2}, {-40, -35}, {6, 8}}));
+        assertEquals(1234, sumIntervals.sumIntervals(new int[][]{{-7, 8}, {-2, 10}, {5, 15}, {2000, 3150}, {-5400, -5338}}));
+        assertEquals(158, sumIntervals.sumIntervals(new int[][]{{-101, 24}, {-35, 27}, {27, 53}, {-105, 20}, {-36, 26},}));
     }
 
 }
