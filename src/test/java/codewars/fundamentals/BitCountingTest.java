fbbda3aacfc0d378f43bae20fc14f55d721d3cac
Christian Seidel <github@parameter5.de>
2022-06-07 23:47:40 +0200

    yet another fundamental challenge

--- src/test/java/codewars/fundamentals/BitCountingTest.java
+++ src/test/java/codewars/fundamentals/BitCountingTest.java
@@ -1,6 +1,5 @@
 package codewars.fundamentals;
 
-import codewars.fundamentals.BitCounting;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
---
+++ src/test/java/codewars/fundamentals/NumberToStringTest.java
@@ -0,0 +1,13 @@
+package codewars.fundamentals;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NumberToStringTest {
+    @Test
+    public void tests() {
+        assertEquals("67", NumberToString.numberToString(67));
+        assertEquals("123", NumberToString.numberToString(123));
+        assertEquals("999", NumberToString.numberToString(999));
+    }
+}
