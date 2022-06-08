2eb3454bc093e04dfc3ef017df4e9f29b0d5e6a4
Christian Seidel <github@parameter5.de>
2022-06-08 22:39:59 +0200

    Count The Divisors

---
+++ src/main/java/codewars/fundamentals/CountTheDivisors.java
@@ -0,0 +1,24 @@
+package codewars.fundamentals;
+
+public class CountTheDivisors {
+/*
+    Count the number of divisors of a positive integer n.
+    Random tests go up to n = 500000.
+    Examples (input --> output)
+        4 --> 3 (1, 2, 4)
+        5 --> 2 (1, 5)
+        12 --> 6 (1, 2, 3, 4, 6, 12)
+        30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+ */
+
+   public long numberOfDivisors(int n) {
+       long count = 0;
+       for (int i = 1; i <= n; i++) {
+           if (n % i == 0) {
+               // System.out.println(i + " × " + n/i);
+               count++;
+           }
+       }
+       return count;
+   }
+}
