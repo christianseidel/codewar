078c3f24cc0df3f6dd303a87da030279b9489d37
Christian Seidel <github@parameter5.de>
2022-06-09 17:25:50 +0200

    Remove First and Last Character

---
+++ src/test/java/codewars/fundamentals/DeleteFirstAndLastCharacterTest.java
@@ -0,0 +1,15 @@
+package codewars.fundamentals;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteFirstAndLastCharacterTest {
+    @Test
+    public void testRemoval() {
+        assertEquals("loquen", DeleteFirstAndLastCharacter.remove("eloquent"));
+        assertEquals("ountr", DeleteFirstAndLastCharacter.remove("country"));
+        assertEquals("erso", DeleteFirstAndLastCharacter.remove("person"));
+        assertEquals("lac", DeleteFirstAndLastCharacter.remove("place"));
+    }
+}
