8617afd64017dadf0843f249f934643aa3bdc58a
Christian Seidel <github@parameter5.de>
2022-06-09 12:33:39 +0200

    Invert Values

---
+++ src/main/java/codewars/fundamentals/InvertValues.java
@@ -0,0 +1,19 @@
+package codewars.fundamentals;
+
+public class InvertValues {
+/*
+    Given a set of numbers, return the additive inverse of each.
+    Each positive becomes negatives, and the negatives become positives.
+        invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+        invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+        invert([]) == []
+ */
+
+    public static int[] invert(int[] array) {
+        int[] output = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            output[i] = -array[i];
+        }
+        return output;
+    }
+}
